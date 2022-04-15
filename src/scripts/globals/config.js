defc05b148c67ab74e92c69a0984ee3f43d6f163
M. Fajar Aditya <mfajaraditya13@gmail.com>
2022-04-15 14:08:57 +0800

    Request data from API & show content from API.

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '2238e6ff91e0f9a69049ef0732a2b246',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,19 @@
+import UrlParse from '../../routes/url.parser';
+import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
+
 const Detail = {
   async render() {
     return `
-                  <h2>Detail Page</h2>
+                 <div id="movie" class="movie"></div>
              `;
   },
 
   async afterRender() {
-
+    const url = UrlParse.parseActiveUrlWithoutCombiner();
+    const movie = await TheMovieDbSource.detailMovies(url.id);
+    const moviesContainer = document.querySelector('#movie');
+    moviesContainer.innerHTML = createMovieDetailTemplate(movie);
   },
 };
 
